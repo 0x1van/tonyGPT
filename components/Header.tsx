b80a7ab42937f327b12fe73835810aeacd1d0e99
Ivan <80768415+0x1van@users.noreply.github.com>
2023-08-03 19:23:26 +0000

    linked changed

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
         </h1>
       </Link>
       <a
-        href="https://vercel.com/templates/next.js/twitter-bio"
+        href="https://tony-gpt-alpha.vercel.app/"
         target="_blank"
         rel="noreferrer"
       >
